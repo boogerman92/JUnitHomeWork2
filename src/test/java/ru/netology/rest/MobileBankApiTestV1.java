0ae181c999ce4c903e0f6e2740f6c7754e33968a
Nazarenko A A <boogerman1992@gmail.com>
2023-04-10 20:01:57 +0400

    test commit

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -1,8 +1,11 @@
 package ru.netology.rest;
 
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.*;
 
 class MobileBankApiTestV1 {
     @Test
@@ -16,6 +19,13 @@ class MobileBankApiTestV1 {
           .get("/demo/accounts")
       // Проверки
       .then()
-          .statusCode(200);
+          .statusCode(200)
+              .contentType(ContentType.JSON)
+              .header("Content-Type", "application/json; charset=UTF-8")
+              .body("",hasSize(3))
+              .body("[1].currency",equalTo("USD"))
+              .body(" every { it.balance >=0 }",is(true))
+              .body(matchesJsonSchemaInClasspath("accounts.schema.json"));
+
     }
 }
